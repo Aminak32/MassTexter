0638e108e08a7293bb10238c5b09dbd61176035e
tommylettieri <tommylettieri@gmail.com>
2017-07-01 15:34:54 -0400

    Main view code cleanup

--- app/src/main/java/lettieri/masstexter/activities/MainActivity.java
+++ app/src/main/java/lettieri/masstexter/activities/MainActivity.java
@@ -17,15 +17,49 @@ import lettieri.masstexter.R;
 
 public class MainActivity extends AppCompatActivity {
     private ArrayList<Group> groups = new ArrayList<>();
-    private ListView lstGroups;
     private ArrayAdapter<Group> adapter;
+
+    private ListView lstGroups;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViews();
+        addGroups();
+        setUp();
 
+    }
+
+    /***
+     * Get the views for the associated layouts
+     */
+    private void findViews() {
+        lstGroups = (ListView)findViewById(R.id.lstGroups);
+    }
+
+    /***
+     * Sets up adapters and on click events
+     */
+    private void setUp() {
+        adapter = new ArrayAdapter<Group>(this, android.R.layout.simple_list_item_1, groups);
+        lstGroups.setAdapter(adapter);
+        lstGroups.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Group selected = (Group)lstGroups.getItemAtPosition(position);
+                Intent intent = new Intent(MainActivity.this, SendMessage.class);
+                intent.putExtra(SendMessage.EXTRA_GROUP_ID, selected.getId());
+                startActivity(intent);
+            }
+        });
+    }
+
+    /***
+     * Add all groups in contacts to the list
+     */
+    private void addGroups() {
         Cursor groupCursor = getContentResolver().query(
                 ContactsContract.Groups.CONTENT_URI,
                 new String[]{
@@ -40,21 +74,5 @@ public class MainActivity extends AppCompatActivity {
             }
             groupCursor.close();
         }
-
-        adapter = new ArrayAdapter<Group>(this, android.R.layout.simple_list_item_1, groups);
-        lstGroups.setAdapter(adapter);
-        lstGroups.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Group selected = (Group)lstGroups.getItemAtPosition(position);
-                Intent intent = new Intent(MainActivity.this, SendMessage.class);
-                intent.putExtra(SendMessage.EXTRA_GROUP_ID, selected.getId());
-                startActivity(intent);
-            }
-        });
-    }
-
-    private void findViews() {
-        lstGroups = (ListView)findViewById(R.id.lstGroups);
     }
 }
