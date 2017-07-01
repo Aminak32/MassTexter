d7affceb2789ef006ea0790dffef79d664e2c653
tommylettieri <tommylettieri@gmail.com>
2017-07-01 04:30:39 -0400

    Initial commit
    Initial commit

---
+++ app/src/main/java/lettieri/masstexter/MainActivity.java
@@ -0,0 +1,13 @@
+package lettieri.masstexter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
