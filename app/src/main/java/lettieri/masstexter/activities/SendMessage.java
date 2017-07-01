ecd63ac94f9cb6bb6c02ce5e20c5b66d27d31f48
tommylettieri <tommylettieri@gmail.com>
2017-07-01 15:31:20 -0400

    Added send functionality
    Added 160 character limit to the text (as per sms spec)
    Added send functionality

--- app/src/main/java/lettieri/masstexter/activities/SendMessage.java
+++ app/src/main/java/lettieri/masstexter/activities/SendMessage.java
@@ -4,7 +4,10 @@ import android.database.Cursor;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -16,9 +19,11 @@ public class SendMessage extends AppCompatActivity {
     public static final String EXTRA_GROUP_ID = "EXTRA_GROUP_ID";
 
     private ArrayList<Contact> contacts = new ArrayList<>();
-    private ListView lstContacts;
     private ArrayAdapter<Contact> adapter;
 
+    private ListView lstContacts;
+    private Button btnSend;
+    private EditText etMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,14 +39,44 @@ public class SendMessage extends AppCompatActivity {
      */
     private void findViews() {
         lstContacts = (ListView)findViewById(R.id.lstContacts);
+        btnSend = (Button)findViewById(R.id.btnSend);
+        etMessage = (EditText)findViewById(R.id.etMessage);
     }
 
     /***
-     * Setup the list adapter
+     * Setup the list adapter and click listeners
      */
     private void setUp() {
         adapter = new ArrayAdapter<Contact>(this, android.R.layout.simple_list_item_1, contacts);
         lstContacts.setAdapter(adapter);
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMessage();
+            }
+        });
+    }
+
+    /***
+     * Grab the text from teh edit text and
+     * send the message to all contacts in the group
+     * it then clears the edit text
+     */
+    private void sendMessage() {
+        String message = etMessage.getText().toString();
+        sendMessage(message);
+        etMessage.getText().clear();
+    }
+
+    /***
+     * send the message to all contacts in the group
+     * @param message is the message to send to everyone
+     */
+    private void sendMessage(String message) {
+        for(Contact c: contacts) {
+            c.sendMessage(message);
+        }
     }
 
     /***
