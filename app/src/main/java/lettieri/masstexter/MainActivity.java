a71a4c421a2c8763ee5b1a780ba30f41d5f9aceb
tommylettieri <tommylettieri@gmail.com>
2017-07-01 04:44:52 -0400

    Added logic for reading all contacts
    Added logic for reading all contacts

--- app/src/main/java/lettieri/masstexter/MainActivity.java
+++ app/src/main/java/lettieri/masstexter/MainActivity.java
@@ -13,6 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SmsManager.getDefault().sendTextMessage("5555555555", null, "Hello world", null, null);
+//        SmsManager.getDefault().sendTextMessage("5555555555", null, "Hello world", null, null);
+        Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,null,null, null);
+        while (phones.moveToNext())
+        {
+            String name=phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+            String phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+            Log.v("TOMTOM", name + " - " + phoneNumber);
+        }
+        phones.close();
     }
 }
