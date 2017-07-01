e04fc7c317b010131ddde5abef9eca7f48d60850
tommylettieri <tommylettieri@gmail.com>
2017-07-01 04:44:19 -0400

    Added contact permission and initial send text logic
    Added contact permission and initial send text logic

--- app/src/main/java/lettieri/masstexter/MainActivity.java
+++ app/src/main/java/lettieri/masstexter/MainActivity.java
@@ -1,7 +1,11 @@
 package lettieri.masstexter;
 
+import android.database.Cursor;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SmsManager.getDefault().sendTextMessage("5555555555", null, "Hello world", null, null);
     }
 }
