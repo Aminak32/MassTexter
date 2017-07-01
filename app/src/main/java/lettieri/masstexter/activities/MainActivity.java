48b9c3030b175f538657cd944ff38b2a9d71f328
tommylettieri <tommylettieri@gmail.com>
2017-07-01 16:22:27 -0400

    Added contact permission to group listing on main activity
    Added contact permission to group listing on main activity

--- app/src/main/java/lettieri/masstexter/activities/MainActivity.java
+++ app/src/main/java/lettieri/masstexter/activities/MainActivity.java
@@ -1,14 +1,20 @@
 package lettieri.masstexter.activities;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -16,6 +22,8 @@ import lettieri.masstexter.Group;
 import lettieri.masstexter.R;
 
 public class MainActivity extends AppCompatActivity {
+    // arbitrary number to determine which permission was granted
+    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 465;
     private ArrayList<Group> groups = new ArrayList<>();
     private ArrayAdapter<Group> adapter;
 
@@ -27,9 +35,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViews();
-        addGroups();
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_CONTACTS},
+                    MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+        } else {
+            addGroups();
+        }
         setUp();
-
     }
 
     /***
@@ -73,6 +86,22 @@ public class MainActivity extends AppCompatActivity {
                 groups.add(new Group(groupCursor.getString(0), groupCursor.getString(1)));
             }
             groupCursor.close();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch(requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS:
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    addGroups();
+                    adapter.notifyDataSetInvalidated();
+                } else {
+                    Toast.makeText(this, "This app requires contact permission, close the app and reopen to allow", Toast.LENGTH_LONG).show();
+                }
+                return;
         }
     }
 }
