91bd56539093da130d9522117673de5ded380be3
tommylettieri <tommylettieri@gmail.com>
2017-07-01 15:01:26 -0400

    Group cleanup and method comments
    Group cleanup and method comments

--- app/src/main/java/lettieri/masstexter/Group.java
+++ app/src/main/java/lettieri/masstexter/Group.java
@@ -9,21 +9,34 @@ public class Group
     private String id;
     private String name;
 
+    /***
+     * Invokes setters
+     * @param id
+     * @param name
+     */
     public Group(String id, String name) {
         this.id = id;
         this.name = name;
     }
 
+    /***
+     *
+     * @return the groups id
+     */
     public String getId() {
         return id;
     }
 
+    /***
+     *
+     * @return the groups name
+     */
     public String getName() {
         return name;
     }
 
     @Override
     public String toString() {
-        return id + " - " + name;
+        return name;
     }
 }
